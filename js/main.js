30e1cae33407116d8497c02b656d618cd8841a42
GodDamnedHohil <korec.illia@gmail.com>
2020-01-28 04:46:24 +0200

    Added section 'Calculator' with adaptation, created custom dropdowns

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$('.issuance').on('click', function() {
+  $('#issuance').toggleClass('calculator__form-select-dropdown-open')
+});
+$('.product-type').on('click', function() {
+  $('#product-type').toggleClass('calculator__form-select-dropdown-open')
+})
+$('.city-of-dispatch').on('click', function() {
+  $('#city-of-dispatch').toggleClass('calculator__form-select-dropdown-open')
+});
+
+
+$('.issuance__options').on('click', function() {
+  let value = $(this).attr('data-value');
+  $('.issuance__value').text(value);
+});
+$('.product-type__options').on('click', function() {
+  let value = $(this).attr('data-value');
+  $('.product-type_value').text(value);
+});
+$('.city-of-dispatch__options').on('click', function() {
+  let value = $(this).attr('data-value');
+  $('.city-of-dispatch__value').text(value);
+});
+
