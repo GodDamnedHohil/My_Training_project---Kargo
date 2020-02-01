2e1d6711e63891f64fc36ac1a9141b4479c8002e
GodDamnedHohil <korec.illia@gmail.com>
2020-02-01 04:26:54 +0200

    Fixed several bugs, edited header for tablets and phones

--- js/main.js
+++ js/main.js
@@ -12,13 +12,19 @@ $('.city-of-dispatch').on('click', function() {
 $('.issuance__options').on('click', function() {
   let value = $(this).attr('data-value');
   $('.issuance__value').text(value);
+  $('#issuance__data').val(value);
+  console.log($('#issuance__data').val());
 });
 $('.product-type__options').on('click', function() {
   let value = $(this).attr('data-value');
-  $('.product-type_value').text(value);
+  $('.product-type__value').text(value);
+  $('#product-type__data').val(value);
+  console.log($('#product-type__data').val())
 });
 $('.city-of-dispatch__options').on('click', function() {
   let value = $(this).attr('data-value');
   $('.city-of-dispatch__value').text(value);
+  $('#city-of-dispatch__data').val(value);
+  console.log($('#city-of-dispatch__data').val())
 });
 
